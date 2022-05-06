eb323bb33ea46181991221b5c49defc9b3fead0d
Antony Quang <antony.quang@googlemail.com>
2022-05-06 21:49:23 +0100

    added contact mailing ability

--- main.py
+++ main.py
@@ -1,20 +1,33 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, request
 import requests
+import smtplib
+from config import senders_email, senders_password, recipients_email
 
 app = Flask(__name__)
 blog_api = "https://api.npoint.io/a7fc2b78ff4a828460da"
 response = requests.get("https://api.npoint.io/a7fc2b78ff4a828460da")
 blog_posts = response.json()
 
+
 @app.route('/')
 def get_all_posts():
-    print(blog_posts)
     return render_template("index.html", posts=blog_posts)
 
 
-@app.route('/contact')
+@app.route('/contact', methods=["GET", "POST"])
 def contact():
-    return render_template("contact.html")
+    if request.method == "POST":
+        entry = request.form
+        name = entry["name"]
+        email = entry["email"]
+        phone = entry["phone"]
+        message = entry["message"]
+        with smtplib.SMTP("smtp.mail.yahoo.com") as connection:
+            connection.starttls()
+            connection.login(user=senders_email, password=senders_password)
+            connection.sendmail(from_addr=senders_email, to_addrs=recipients_email, msg=f"Subject:New Message from your blog!\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage: {message}")
+        return render_template("contact.html", msg_sent=True)
+    return render_template("contact.html", msg_sent=False)
 
 
 @app.route('/about')
@@ -28,6 +41,7 @@ def post(blog_id):
         if blog_post["id"] == int(blog_id):
             requested_post = blog_post
             return render_template("post.html", post=requested_post)
+
 
 
 if __name__ == "__main__":
